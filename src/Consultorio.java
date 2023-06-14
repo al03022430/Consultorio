ce5d3a0a10f1485c8889710bbaf6d8342c96de93
Roland <al03022430@tecmilenio.mx>
2023-06-14 15:42:06 -0600

    Se termina clase Consultorio.java

--- src/Consultorio.java
+++ src/Consultorio.java
@@ -8,8 +8,228 @@ public class Consultorio {
     private static ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
     public static HashMap<Integer, Medico> listaMedicos = new HashMap<Integer, Medico>();
     public static HashMap<Integer, Paciente> listaPacientes = new HashMap<Integer, Paciente>();
-    public static HashMap<Integer, Recepcion> listaRecepcionistas = new HashMap<Integer, Recepcion>();
+    public static HashMap<Integer, Recepcion> listaRece = new HashMap<Integer, Recepcion>();
     public static HashMap<Integer, Cita> listaCitas = new HashMap<Integer, Cita>();
     private static Scanner teclado = new Scanner(System.in);
-    
+
+    public static void main(String[] args) {
+        crearlistas (listaMedicos, listaPacientes, listaRece);
+        if (validarAcceso()) {
+            System.out.println("Usuario o contraseña incorecto");
+            menu();
+        }
+        else
+            System.out.println("\nUsuario no autorizado.");
+
+        System.out.println("Gracias por utilizar nuesta aplicacion");
+    }
+    public static void crearlistas (HashMap listaMedicos, HashMap listaPacientes, HashMap listaRece) {
+
+        String inputFilenameMedicos = "src/CitaMedica/Medicos.cvs";
+        String inputFilenamePacientes = "src/CitaMedica/Pacientes.cvs";
+        String inputFilenameRecepcionista = "src/CitaMedica/Recepcionistas.cvs";
+        BufferedReader bufferedReader = null;
+        String Nombre = "";
+        String Especialidad = "";
+        try {
+            bufferedReader = new BufferedReader(new FileReader(inputFilenameMedicos));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                int coma = line.indexOf(',');
+                Nombre = line.substring(0, coma);
+                Especialidad = line.substring(coma+1, line.length());
+                int id = listaMedicos.size();
+                listaMedicos.put(id+1, new Medico(Nombre,Especialidad));
+            }
+        } catch(IOException e) {
+            System.out.println("IOException catched while reading: " + e.getMessage());
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException catched while closing: " + e.getMessage());
+            }
+        }
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(inputFilenamePacientes));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                Nombre = line;
+                int id = listaPacientes.size();
+                listaPacientes.put(id+1, new Paciente(line));
+            }
+        } catch(IOException e) {
+            System.out.println("IOException catched while reading: " + e.getMessage());
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException catched while closing: " + e.getMessage());
+            }
+        }
+        try {
+            bufferedReader = new BufferedReader(new FileReader(inputFilenameRecepcionista));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                int coma = line.indexOf(',');
+                Nombre = line.substring(0, coma);
+                Especialidad = line.substring(coma+1, line.length());
+                int id = listaMedicos.size();
+                listaRece.put(id+1, new (Nombre,Especialidad));
+            }
+        } catch(IOException e) {
+            System.out.println("IOException catched while reading: " + e.getMessage());
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException catched while closing: " + e.getMessage());
+            }
+        }
+    }
+    private static boolean validarAcceso( ) {
+
+        usuarios.add(new Usuario("Administrator", "1234"));
+
+
+        System.out.println("CLINICA AGENDA CITA");
+        System.out.print("Nombre de usuario: ");
+        String nombre = teclado.nextLine();
+        System.out.print("Contraseña: ");
+        String contraseña = teclado.nextLine();
+        Usuario admin = new Usuario(nombre, contraseña);
+        return usuarios.contains(admin);
+    }
+    private static void menu(){
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion;
+
+        while (!salir) {
+            System.out.println("\n Seleccione la Opcion deseada");
+            System.out.println("1. Alta Doctor");
+            System.out.println("2. Alta Paciente");
+            System.out.println("3. Alta Recepcionista");
+            System.out.println("4. Alta Cita");
+            System.out.println("5. Salir");
+            try {
+                System.out.println("Escribe una de las opciones:");
+                opcion = sn.nextInt();
+                int id = 0;
+                String nombre = "";
+                switch (opcion) {
+                    case 1:
+                        String especialidad = "";
+                        System.out.print("Nombre Doctor: ");
+                        nombre = teclado.nextLine();
+                        System.out.print("Especialidad: ");
+                        especialidad = teclado.nextLine();
+                        id = listaMedicos.size();
+                        listaMedicos.put(id+1, new Medico(nombre,especialidad));
+                        break;
+                    case 2:
+                        System.out.print("Nombre Paciente: ");
+                        nombre = teclado.nextLine();
+                        id = listaPacientes.size();
+                        listaPacientes.put(id+1, new Paciente(nombre));
+                        break;
+                    case 3:
+                        String especialidad = "";
+                        System.out.print("Nombre Recepcionista: ");
+                        nombre = teclado.nextLine();
+                        System.out.print("Especialidad: ");
+                        especialidad = teclado.nextLine();
+                        id = listaRece.size();
+                        listaRece.put(id+1, new Recepcionista(nombre,especialidad));
+                        break;
+                    case 4:
+                        int medico;
+                        int paciente;
+                        String fecha;
+                        String motivo;
+                        boolean valid = false;
+                        do{
+                            System.out.println("AGENDA DE DOCTORES");
+                            for (Iterator<Map.Entry<Integer, Medico>> entries = listaMedicos.entrySet().iterator(); entries.hasNext(); ) {
+                                Map.Entry<Integer, Medico> entry = entries.next();
+                                String output = String.format("%s. %s", entry.getKey(), entry.getValue());
+                                System.out.println(output);
+                            }
+                            System.out.print("Seleccione Doctor: ");
+                            medico = Integer.parseInt(teclado.nextLine());
+                            valid = listaMedicos.containsKey(medico);
+                        }while(valid != true);
+                        valid = false;
+                        do{
+                            System.out.println("AGENDA DE PACIENTES");
+                            for (Iterator<Map.Entry<Integer, Paciente>> entries = listaPacientes.entrySet().iterator(); entries.hasNext(); ) {
+                                Map.Entry<Integer, Paciente> entry = entries.next();
+                                String output = String.format("%s. %s", entry.getKey(), entry.getValue());
+                                System.out.println(output);
+                            }
+                            System.out.print("Seleccione Paciente: ");
+                            paciente = Integer.parseInt(teclado.nextLine());
+                            valid = listaPacientes.containsKey(paciente);
+                        }while(valid != true);
+                        valid = false;
+                        Date testDate = null;
+                        do{
+                            System.out.print("Seleccione Fecha: ");
+                            System.out.println("Introduzca la fecha con formato yyyy-MM-ddHH:mm:ss");
+                            Scanner sc = new Scanner(System.in);
+
+                            fecha = sc.nextLine();
+                            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-ddHH:mm:ss");
+                            String date = fecha;
+                            try{
+                                testDate = df.parse(date);
+                                valid = true;
+                                //System.out.println("Ahora hemos creado un objeto date con la fecha indicada, "+testDate);
+                            } catch (Exception e){ System.out.println("Formato invalido para Fecha intente de nuevo!!");}
+
+                            if (df != null) {
+                                if(!df.format(testDate).equals(date)) {
+                                    System.out.println("Formato invalido para Fecha intente de nuevo!!");
+                                } else{
+                                    //System.out.println("valid date");
+                                    valid = true;
+                                }
+                            }
+                        }while(valid != true);
+                        System.out.print("Motivo de consulta: ");
+                        motivo = teclado.nextLine();
+                        id = listaCitas.size();
+                        String Medico = listaMedicos.get(medico).toString();
+                        int coma = Medico.indexOf(':');
+                        Medico = Medico.substring(coma+2, Medico.length()).toString();
+                        coma = Medico.indexOf(':');
+                        Medico = Medico.substring(0, coma).toString();
+                        Medico = Medico.substring(0, Medico.length()-13);
+                        String Paciente = listaPacientes.get(paciente).toString();
+                        coma = Paciente.indexOf(':');
+                        Paciente = Paciente.substring(coma+2, Paciente.length()).toString();
+                        listaCitas.put(id+1, new Cita(Medico, Paciente, testDate.toString(), motivo));
+                        System.out.print(listaCitas.get(id+1));
+                        break;
+                    case 4:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 4");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
+        }
+    }
 }
