93f72fb66dafdcddb9d3c22c419f2187146da04f
Roland <al03022430@tecmilenio.mx>
2023-06-14 14:43:45 -0600

    Inicio de programa Clinica, declaracion de clases

---
+++ src/Consultorio.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Consultorio {
+    private static ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+    public static HashMap<Integer, Medico> listaMedicos = new HashMap<Integer, Medico>();
+    public static HashMap<Integer, Paciente> listaPacientes = new HashMap<Integer, Paciente>();
+    public static HashMap<Integer, Recepcion> listaRecepcionistas = new HashMap<Integer, Recepcion>();
+    public static HashMap<Integer, Cita> listaCitas = new HashMap<Integer, Cita>();
+    private static Scanner teclado = new Scanner(System.in);
+    
+}
